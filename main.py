2846841c390e3654a5f820ac61e9269f98a976e1
Izuch <izuchukwu@69@gmail.com>
2023-07-29 01:54:30 -0230

    minor fine-tuning

--- main.py
+++ main.py
@@ -6,18 +6,18 @@ from nltk.tokenize import RegexpTokenizer
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense, Activation
 from tensorflow.keras.optimizers import RMSprop
+from justifytext import justify
 
 # read data from dataset
 with open('assets/fairytale_dataset.txt', 'r', encoding='utf-8') as dataset:
     data = dataset.read()
 
-data = data[:500000]
+data = data[:700000]
 
 tokenizer = RegexpTokenizer(r"\w+")
 tokens = tokenizer.tokenize(data.lower())
 unique_tokens = sorted(list(set(tokens)))
 token_indexes = {token: index for index, token in enumerate(unique_tokens)}
-# print(token_indexes)
 
 # setting up context
 '''
@@ -36,12 +36,10 @@ CONTEXT_LENGTH = 10
 contexts = []
 targets = []
 
-# populate the contexts and targets arrays
+populate the contexts and targets arrays
 for t in range(len(tokens) - CONTEXT_LENGTH):
     contexts.append(tokens[t:t + CONTEXT_LENGTH])
     targets.append(tokens[t + CONTEXT_LENGTH])
-print(contexts[:100])
-print(targets[:100])
 
 # make numpy arrays filled with zeroes for the contexts and targets
 
@@ -55,28 +53,25 @@ for i, words in enumerate(contexts):
         contexts_np_array[i, j, token_indexes[word]] = 1
     targets_np_array[i, token_indexes[targets[i]]] = 1
 
-print(targets_np_array.shape)
-print('\n\n\n')
-print(contexts_np_array.shape)
-print('training model')
 
-# Assembling the Neural Network
-# model = Sequential()
-# model.add(LSTM(128, input_shape=(CONTEXT_LENGTH, len(unique_tokens))))
-# model.add(Dense(len(unique_tokens)))
-# model.add(Activation('softmax'))
-#
-# model.compile(loss='categorical_crossentropy', optimizer=RMSprop(learning_rate=0.01), metrics=["accuracy"])
-# model.fit(contexts_np_array, targets_np_array, batch_size=128, epochs=10, shuffle=True)
-#
-# model.save('assets/fairytaleAiV4-1000000.model')
+Assembling the Neural Network
+model = Sequential()
+model.add(LSTM(128, input_shape=(CONTEXT_LENGTH, len(unique_tokens)), return_sequences=True))
+model.add(LSTM(128))
+model.add(Dense(len(unique_tokens)))
+model.add(Activation('softmax'))
+
+model.compile(loss='categorical_crossentropy', optimizer=RMSprop(learning_rate=0.01), metrics=["accuracy"])
+model.fit(contexts_np_array, targets_np_array, batch_size=128, epochs=10, shuffle=True)
+
+model.save('assets/fairytaleAiV5-1000000.model')
 
 
 #
 #
-model = tf.keras.models.load_model('assets/fairytaleAiV4-1000000.model')
-# model.fit(contexts, targets, epochs=200, batch_size=128)
-# model.save('assets/fairytaleAiV3-1000000-V2.model')
+# model = tf.keras.models.load_model('assets/fairytaleAiV5-1000000.model')
+# # model.fit(contexts, targets, epochs=10, batch_size=128)
+# # model.save('assets/fairytaleAiV5-1000000-V2.model')
 
 
 def predict_next_word(input_text, n_best):
@@ -84,17 +79,11 @@ def predict_next_word(input_text, n_best):
     X = np.zeros((1, CONTEXT_LENGTH, len(unique_tokens)))
     for i, word in enumerate(input_text.split()):
         X[0, i, token_indexes[word]] = 1
-
     predictions = model.predict(X)[0]
     return np.argpartition(predictions, -n_best)[-n_best:]
 
 
-pre = predict_next_word('He wished to marry her because he', 5)
-for index in pre:
-    print(unique_tokens[index])
-
-
-def generate_text(input_text, n_words, creativity=3):
+def generate_text(input_text, n_words, creativity=5):
     word_sequence = input_text.split()
     current = 0
     for _ in range(n_words):
@@ -105,8 +94,10 @@ def generate_text(input_text, n_words, creativity=3):
             choice = random.choice(unique_tokens)
         word_sequence.append(choice)
         current += 1
-    return " ".join(word_sequence)
+    return justify(" ".join(word_sequence), 100)
 
 
-print(generate_text("said the king you must not be in such a hurry", 100, 10))
+out = generate_text("the king you must not be in such a hurry", 10000, 10)
+for line in out:
+    print(line)
 
